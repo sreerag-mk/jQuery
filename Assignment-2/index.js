91b30af1e56396f9a9b98c97b3c829a8f04ab372
sreerag <sreeragmanoj07.com>
2023-08-23 13:50:15 +0530

    commit after completing assignment-2

---
+++ Assignment-2/index.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    const arrayImg = ["img_1.jpg", "img_2.jpg", "img_3.jpg", "img_4.jpg", "img_5.jpg"]
+    let startingIndex = 0;
+    function sliderChecker() {
+        if (startingIndex === 0){
+            $("#img1").removeClass("normal")
+            $("#img2").addClass("normal")
+            $("#img3").addClass("normal")
+            $("#img4").addClass("normal")
+            $("#img5").addClass("normal")
+        }
+        if (startingIndex === 1){
+            $("#img2").removeClass("normal")
+            $("#img1").addClass("normal")
+            $("#img3").addClass("normal")
+            $("#img4").addClass("normal")
+            $("#img5").addClass("normal")
+        }
+        if (startingIndex === 2){
+            $("#img3").removeClass("normal")
+            $("#img2").addClass("normal")
+            $("#img1").addClass("normal")
+            $("#img4").addClass("normal")
+            $("#img5").addClass("normal")
+        }
+        if (startingIndex === 3){
+            $("#img4").removeClass("normal")
+            $("#img1").addClass("normal")
+            $("#img2").addClass("normal")
+            $("#img3").addClass("normal")
+            $("#img5").addClass("normal")
+        }
+        if (startingIndex === 4){
+            $("#img5").removeClass("normal")
+            $("#img2").addClass("normal")
+            $("#img3").addClass("normal")
+            $("#img4").addClass("normal")
+            $("#img1").addClass("normal")
+        }
+    }
+    setInterval(function silderShow() {
+        $("#img1").click(function(){
+            startingIndex = 0;
+            $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+            sliderChecker()
+        })
+        $("#img2").click(function(){
+            startingIndex = 1;
+            $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+            sliderChecker()
+        })
+        $("#img3").click(function(){
+            startingIndex = 2;
+            $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+            sliderChecker()
+        })
+        $("#img4").click(function(){
+            startingIndex = 3;
+            $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+            sliderChecker()
+        })
+        $("#img5").click(function(){
+            startingIndex = 4;
+            $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+            sliderChecker()
+        })
+        if (startingIndex >= arrayImg.length) {
+            startingIndex = 0;
+        }
+
+        sliderChecker();
+        
+       
+        $("#image").attr("src", `images/${arrayImg[startingIndex]}`);
+        startingIndex += 1
+    }, 3000)
+})
